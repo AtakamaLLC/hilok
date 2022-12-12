4c53af22c8a5f432f27c8bcba1dda241f11bb543
erik <erik@q32.com>
2022-12-12 09:27:10 -0500

    whatevertrythis

--- src/hilok.cpp
+++ src/hilok.cpp
@@ -255,7 +255,7 @@ void HiLok::erase_safe(std::shared_ptr<HiKeyNode> &ref) {
 void HiLok::erase_unsafe(std::shared_ptr<HiKeyNode> &ref) {
     // lazy speedup, there can be lots of refs (parent->child ref, caller refs, etc.)
     // but if there are too many, we know the exclusive cannot work and is not worth even trying
-    if (ref.use_count() <= 7) {
+    if (ref.use_count() <= 10) {
         // maybe no one else is using it?
         if (ref->m_mut.try_lock()) {
             // we now have an exclusive lock, so we really know nobody is using it
