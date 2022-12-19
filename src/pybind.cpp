8384e1aa9861ec765fb5c4de2a17f9f812378e00
erik <erik@q32.com>
2022-12-19 10:07:21 -0500

    move enum up

--- src/pybind.cpp
+++ src/pybind.cpp
@@ -7,6 +7,16 @@ namespace py = pybind11;
 PYBIND11_MODULE(hilok, m)
 {   
     m.doc() = "Hierarchical lock manager";
+
+    py::enum_<HiFlags>(m, "HiLokFlags", py::arithmetic())
+        .value("STRICT", HiFlags::STRICT)
+        .value("LOOSE_READ_UNLOCK", HiFlags::LOOSE_READ_UNLOCK)
+        .value("LOOSE_WRITE_UNLOCK", HiFlags::LOOSE_WRITE_UNLOCK)
+        .value("RECURSIVE_WRITE", HiFlags::RECURSIVE_WRITE)
+        .value("RECURSIVE_ONEWAY", HiFlags::RECURSIVE_ONEWAY)
+        .value("RECURSIVE", HiFlags::RECURSIVE)
+        .value("LOOSE_UNLOCK", static_cast<HiFlags>(HiFlags::LOOSE_READ_UNLOCK + HiFlags::LOOSE_WRITE_UNLOCK));
+
     py::class_<HiLok, std::shared_ptr<HiLok>>(m, "HiLok")
         .def(py::init<>())
         .def(py::init<char>(), py::arg("sep") = '/')
@@ -42,15 +52,6 @@ PYBIND11_MODULE(hilok, m)
         .def("__enter__", [](std::shared_ptr<HiHandle> hh) {return hh;})
         .def("__exit__", [](std::shared_ptr<HiHandle> hh, const py::object &, const py::object &, const py::object &) { hh->release(); })
         ;
-
-    py::enum_<HiFlags>(m, "HiLokFlags", py::arithmetic())
-        .value("STRICT", HiFlags::STRICT)
-        .value("LOOSE_READ_UNLOCK", HiFlags::LOOSE_READ_UNLOCK)
-        .value("LOOSE_WRITE_UNLOCK", HiFlags::LOOSE_WRITE_UNLOCK)
-        .value("RECURSIVE_WRITE", HiFlags::RECURSIVE_WRITE)
-        .value("RECURSIVE_ONEWAY", HiFlags::RECURSIVE_ONEWAY)
-        .value("RECURSIVE", static_cast<HiFlags>(HiFlags::RECURSIVE))
-        .value("LOOSE_UNLOCK", static_cast<HiFlags>(HiFlags::LOOSE_READ_UNLOCK + HiFlags::LOOSE_WRITE_UNLOCK));
 
     py::register_exception<HiErr>(m, "HiLokError", PyExc_TimeoutError);
 
